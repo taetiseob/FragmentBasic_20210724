e8dd9ca86d517ff0cfac3d5dd9641e5a0c40863a
김태섭 <kyoer234@gmail.com>
2021-07-24 16:04:02 +0900

    프래그먼트의 동작 관련 코드 작성   - 어떤 XML을 쓸건지 : OnCreateView   - 실제로 무슨 동작 : onActivityCreated

---
+++ app/src/main/java/com/nepplus/fragmentbasic_20210724/fragments/FirstFragment.kt
@@ -0,0 +1,30 @@
+package com.nepplus.fragmentbasic_20210724.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nepplus.fragmentbasic_20210724.R
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        logBtn.setOnClickListener {
+            Log.d("FirstFrag","로그 버튼 눌림")
+        }
+
+    }
+}
